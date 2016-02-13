687d10e7f0b6c5a7e07fa60d3b98254d758b42f4
Daniel Faria <danielfnfaria@gmail.com>
2016-02-13 12:54:54 -0200

    add new line

--- ampRender.php
+++ ampRender.php
@@ -2,6 +2,7 @@
 if (!isset($argc))
   {
   //==== Default for web server =============================================================
+  //==== Default for web server =============================================================
   $urlToTranslate = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
   }
 else
