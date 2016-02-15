38a169355cece961a62f2ef612e2ce25ffcdbbfa
Luiz Henrique Lugato <lugato@Luizs-MacBook-Pro.local>
2016-02-15 16:35:30 -0200

    sync com repositorio anterior

--- ampRender.php
+++ ampRender.php
@@ -2,8 +2,6 @@
 if (!isset($argc))
   {
   //==== Default for web server =============================================================
-  //==== Default for web server =============================================================
-  //==== Default for web server =============================================================
   $urlToTranslate = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
   }
 else
@@ -27,8 +25,18 @@ else
   #=== youtube ==========================================================================================================================
   $urlToTranslate = "int.amp.exame.abril.com.br?url=/tecnologia/noticias/ondas-gravitacionais-previstas-por-einstein-sao-descobertas";
 
+
+  #=== Twitter ==========================================================================================================================
+  $urlToTranslate = "int.exame.abril.com.br/negocios/noticias/ceo-do-twitter-doa-parte-de-suas-acoes-aos-seus-funcionarios";
+
+  #=== Instagram ==========================================================================================================================
+  $urlToTranslate = "int.exame.abril.com.br/marketing/noticias/estilista-marc-jacobs-procura-por-modelos-no-instagram";
+
   #=== Facebook =========================================================================================================================
   $urlToTranslate = "int.amp.exame.abril.com.br/tecnologia/noticias/e-uma-das-maiores-descobertas-da-ciencia-diz-zuckerberg";
+
+ #=== Galeria ==========================================================================================================================
+  $urlToTranslate = "int.amp.exame.abril.com.br?url=/marketing/noticias/cvc-dara-10-anos-de-ferias-gratis-para-10-clientes";
 
   }
 
@@ -41,7 +49,7 @@ Class ExameAmp
   private $materiaJson;
   private $tipoRecurso;
   private $template;
-  private $templatePath="/opt/abril/googleamp/templates/"; #$_SERVER['DOCUMENT_ROOT']."/"."templates/";
+  private $templatePath="templates/"; #$_SERVER['DOCUMENT_ROOT']."/"."templates/";
   private $url;
 
   public function __construct($url,$debugMode)
@@ -53,6 +61,14 @@ Class ExameAmp
         {
           $this->LoadTemplate();
           $this->SetYoutubeVideo();
+          $this->SetTwitter();
+          $this->SetInstagram();
+          $this->SetFacebook();
+          $this->SetGallery();
+
+          $this->InjectBanner();
+
+
           $this->Render();
           if ($debugMode){$this->Debug();}
         }
@@ -256,6 +272,154 @@ Class ExameAmp
 
     $this->SetHtmlBody($content);
   }
+
+  private function SetFacebook()
+  {
+    $content = $this->GetHtmlBody();
+
+    #== Clear Facebook JS =====================================================================================================
+    $clearFacebookScript = "<script>\(function.*?facebook.*?facebook.*?>";
+    $content = preg_replace("/$clearFacebookScript/","",$content);
+
+    #== Get Facebook ID =======================================================================================================
+    $regerxFbUrl = '<div class.*?fb\-post.*?href.*?\"(.*?)\".*?>';
+    preg_match_all("/$regerxFbUrl/",$content,$vectFacebookUrls);
+
+    #== Parse each facebook that exist in the page with the new call to amp facebook ===========================================
+    $facebookTemplate = '<amp-facebook width=486 height=657 layout="responsive" data-href="<@FACEBOOK-URL>"> </amp-facebook>';
+    for ($x=0; $x<=count($vectFacebookUrls[0])-1; $x++)
+      {
+        $regexToChange    = $vectFacebookUrls[0][$x];
+        $particleToInject = $facebookTemplate;
+        $particleToInject = preg_replace("/<@FACEBOOK-URL>/",$vectFacebookUrls[1][$x],$particleToInject);
+        $content          = preg_replace("#$regexToChange#",$particleToInject,$content);
+      }
+
+    #== Clear FB blockquote =================================================================================================
+    $content = preg_replace("/\r\n+|\r+|\n+|\t+/i","<##QB##>",$content);
+    $regexFlatContent = '<div class.*?fb\-xfbml\-parse\-ignore.*?>.*?<blockquote.*?<\/a><\/blockquote>.*?<##QB##><##QB##><\/div><##QB##><\/div>';
+    $content = preg_replace("/$regexFlatContent/","",$content);
+    $content = preg_replace("/<##QB##>/","\n",$content);
+
+    $this->SetHtmlBody($content);
+  }
+
+  private function SetTwitter()
+   {
+     $content = $this->GetHtmlBody();
+     $content = str_replace('<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>', '', $content);
+
+     $regexTwitter = '<blockquote class=\"twitter.*?status\/(.*?)\">.*?<\/blockquote>';
+
+     preg_match_all("#$regexTwitter#s",$content,$vectTwitter); #-> o # substituiu o / da regex
+
+     $particleTemplateTwitter = '<amp-twitter width=486 height=657 layout="responsive" data-tweetid="<@TWITTER>" data-cards="hidden">';
+
+     for ($x=0; $x<=count($vectTwitter[0])-1; $x++)
+     {
+       $regexToChange    = $vectTwitter[0][$x];
+       $particleToInject = $particleTemplateTwitter;
+       $particleToInject = preg_replace("/<@TWITTER>/",$vectTwitter[1][$x],$particleToInject);
+       $content          = str_replace("$regexToChange",$particleToInject,$content);
+     }
+     $this->SetHtmlBody($content);
+   }
+
+  private function SetInstagram()
+   {
+     $content = $this->GetHtmlBody();
+     $content = str_replace('<script async defer src="//platform.instagram.com/en_US/embeds.js"></script>', '',$content);
+     $regexInstagram = '<blockquote class=\"instagram.*?p/(.*?)\/\".*?<\/blockquote>';
+     //'<blockquote class=\"twitter.*?status\/(.*?)\">.*?<\/blockquote>';
+
+     preg_match_all("#$regexInstagram#s",$content,$vectInstagram); #->o # substituiu o / da regex
+
+     $particleTemplateInstagram = '<amp-instagram data-shortcode="<@INSTAGRAM>" width="400" height="400" layout="responsive"></amp-instagram>';
+
+     for ($x=0; $x<=count($vectInstagram[0])-1; $x++)
+     {
+         $regexToChange    = $vectInstagram[0][$x];
+         $particleToInject = $particleTemplateInstagram;
+         $particleToInject = preg_replace("/<@INSTAGRAM>/",$vectInstagram[1][$x],$particleToInject);
+         $content          = str_replace("$regexToChange",$particleToInject,$content);
+     }
+     $this->SetHtmlBody($content);
+   }
+
+
+//<p>.*?<conteudo slug="(.*?)\" tipo_recurso=\"(.*?)\".*?\/><\/p>
+
+  private function SetGallery()
+   {
+
+
+     $content = $this->GetHtmlBody();
+
+     $regexGallery = '<conteudo slug="(.*?)\" tipo_recurso=\"(.*?)\".*?\/>';
+     //'<blockquote class=\"twitter.*?status\/(.*?)\">.*?<\/blockquote>';
+
+     preg_match_all("#$regexGallery#m",$content,$vectGallery); #->o # substituiu o / da regex
+
+     if ($vectGallery[2][0] === "galeria multimidia"){
+
+
+        $apiGallery = file_get_contents('http://api.exame.abril.com.br/v2/materias/'.$vectGallery[1][0]);
+        $apiGalleryContent = json_decode($apiGallery,true);
+
+        //Gallery
+        $particleTemplateGallery = '<div class="gallery"><h2>'.$apiGalleryContent['titulo'].'</h2><amp-carousel width="auto" height="331">';
+
+        foreach($apiGalleryContent['midias'] as $k=>$midia){
+
+            $particleTemplateGallery .= '<amp-img src="'.$midia['transformacoes']['590'].'" width="590" height="331" on="tap:lightbox'.$k.'"></amp-img>';
+        }
+
+        $particleTemplateGallery .= '</amp-carousel>';
+
+
+        //Lightbox
+        foreach($apiGalleryContent['midias'] as $k=>$midia){
+
+          $midia['corpo'] = strip_tags($midia['corpo'], '<p><a>');
+
+          $particleTemplateGallery .= '
+          <amp-lightbox id="lightbox'.$k.'" class="lightbox1" layout="nodisplay">
+          <div class="lightbox1-content">
+            <div class="image-credit">'.$midia['creditos'].'</div>
+            <amp-img id="img'.$k.'" src="'.$midia['transformacoes']['590'].'" width="590" height="331" layout="responsive" on="tap:lightbox'.$k.'.close"></amp-img>
+            <div class="image-caption">'.$midia['alt'].'</div>
+            '.$midia['corpo'].'
+          </div>
+        </amp-lightbox>';
+        }
+
+        $particleTemplateGallery .= '</div>';
+
+
+        $content = str_replace($vectGallery[0][0],$particleTemplateGallery,$content);
+        $this->SetHtmlBody($content);
+     }
+   }
+
+   private function InjectBanner()
+   {
+     $content = $this->GetHtmlBody();
+
+     $banner = "
+     <!-- Rectangle -->
+     <amp-ad width=300 height=250
+     type=\"doubleclick\"
+     data-slot=\"/9287/exame/home\"
+     json='{\"targeting\":{\"position\":[\"amp\"]}}' >
+     </amp-ad>
+     ";
+
+     $content = preg_replace("/<p>/"                    ,"<p class='ampBanner'>"  ,$content,4);
+     $content = preg_replace("/<p class='ampBanner'>/"  ,"<p>"                    ,$content,3);
+     $content = preg_replace("/<p class='ampBanner'>/"  ,"$banner<p>"             ,$content);
+
+     $this->SetHtmlBody($content);
+   }
 
   private function Debug()
   {
